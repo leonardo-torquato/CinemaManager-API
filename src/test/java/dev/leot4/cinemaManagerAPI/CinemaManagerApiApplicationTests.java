3dfa81363c0b7f3520fb4fde670a8b5b24a277e7
leonardo torquato <leonardotorquato49@gmail.com>
2023-02-16 12:19:23 -0300

    Inicialização do projeto Spring e conexão com o banco de dados MongoDB

---
+++ src/test/java/dev/leot4/cinemaManagerAPI/CinemaManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.leot4.cinemaManagerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
